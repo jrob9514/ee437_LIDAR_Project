b93d15a5258724973f1c6012407fb22e93abaf6d
Jacob Robinson <jmrobinson3938@gmail.com>
2020-11-09 18:49:23 -0600

    Test 2

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Jacob 2')
+    print_hi('Jacob and Praise')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
