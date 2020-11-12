00cb0c4975104f4f56201ac30d2701c37d011417
Jacob Robinson <jmrobinson3938@gmail.com>
2020-11-11 18:10:06 -0600

    Added mor functions to give insertion points for GUI code

--- main.py
+++ main.py
@@ -11,7 +11,40 @@ lidar_program_running = True
 lidar_sensor = PyRPlidar()
 
 # LIDAR motor speed
-lidar_motor_speed = 250
+lidar_motor_speed = 10
+
+
+def setup_sensor():
+    # Setup LIDAR
+    lidar_sensor.connect(port="/dev/ttyUSB0", baudrate=115200, timeout=3)
+
+    # Set LIDAR Motor
+    lidar_sensor.set_motor_pwm(lidar_motor_speed)
+    time.sleep(2)
+
+    return
+
+
+def setup_gui():
+    # INSERT Setup elements of GUI
+
+    return
+
+
+def stop_sensor():
+    # Perform actions required to stop the LIDAR sensor
+    lidar_sensor.stop()
+    lidar_sensor.set_motor_pwm(0)
+
+    lidar_sensor.disconnect()
+
+    return
+
+
+def stop_gui():
+    # INSERT code to stop the GUI gracefully
+
+    return
 
 
 def collect_scan_rotation(input_scan_generator):
@@ -30,27 +63,61 @@ def collect_scan_rotation(input_scan_generator):
 
 
 def stop_program():
+    # INSERT Potential Stop button handler
     global lidar_program_running
-
     lidar_program_running = False
 
 
+def increase_motor_speed():
+    # INSERT Increment the motor speed of the LIDAR (Potential button handler)
+    global lidar_motor_speed
+
+    lidar_sensor.stop()
+
+    # rpm by which the motor speed is increasing
+    increment = 10
+
+    if (lidar_motor_speed + increment) <= 500:
+        lidar_motor_speed += increment
+
+    lidar_sensor.set_motor_pwm(lidar_motor_speed)
+
+    lidar_sensor.start_scan()
+
+    return
+
+
+def decrease_motor_speed():
+    # INSERT Decrement the motor speed of the LIDAR (Potential button handler)
+    global lidar_motor_speed
+
+    # rpm by which the motor speed is increasing
+    decrement = 10
+
+    if (lidar_motor_speed - decrement) >= 0:
+        lidar_motor_speed -= decrement
+
+    lidar_sensor.set_motor_pwm(lidar_motor_speed)
+
+    return
+
+
 def draw_points(input_points):
+    # INSERT updates points drawn on GUI
     for point in input_points:
         print(point)
 
 
 if __name__ == "__main__":
-    global lidar_program_running
-    global lidar_sensor
-    global lidar_motor_speed
+    # global lidar_program_running
+    # global lidar_sensor
+    # global lidar_motor_speed
 
-    # Setup LIDAR
-    lidar_sensor.connect(port="/dev/ttyUSB0", baudrate=115200, timeout=3)
+    # Call setup code for sensor
+    setup_sensor()
 
-    # Set LIDAR Motor
-    lidar_sensor.set_motor_pwm(250)
-    time.sleep(2)
+    # Call setup code for GUI
+    setup_gui()
 
     # Create scan generator to grab LIDAR scan data
     scan_generator = lidar_sensor.force_scan()
@@ -66,9 +133,6 @@ if __name__ == "__main__":
         draw_points(lidar_points)
 
         if rotations >= 10:
-            lidar_program_running = False
+            stop_program()
 
-    lidar_sensor.stop()
-    lidar_sensor.set_motor_pwm(0)
-
-    lidar_sensor.disconnect()
+    stop_sensor()
