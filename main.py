6ffa737657f18796e06338db138c9a4c334c5630
praiseisaac <daramolapraise@gmail.com>
2020-12-05 22:39:53 -1000

    Added save menu

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from tkinter import *
 from tkinter import font as tkFont
 
 import threading
+import subprocess
 
 from matplotlib.backends.backend_tkagg import (
     FigureCanvasTkAgg, NavigationToolbar2Tk)
@@ -35,7 +36,7 @@ window = Tk()
 figure = plt.Figure(figsize=(30, 30), dpi=100)
 ax = figure.add_subplot(111, projection='polar')
 export = False
-
+save_menu = ""
 
 def scan():
     # Function for generating lidar scan data
@@ -86,18 +87,14 @@ def stop_sensor():
 def draw_points():
     # Function for updating the GUI canvas
     global export
-
+    data_export = []
     try:
         # Grab the first data point in the queue
         scan_points = data_queue.get()
-
+        
         if scan_points:
             # Clear the polar plot
             ax.clear()
-            if export:
-                filename = "export_" + datetime.datetime.now().strftime("%d-%m-%Y_%I:%M_%s") + ".csv"
-                export_file = open(filename, "w")
-                export_file.write(f'Angle,Distance \n')
             # Loop through the list of data points
             for angle in range(360):
                 # Assign a distance for each angle
@@ -105,16 +102,23 @@ def draw_points():
                 # Convert angle from degrees to radians
                 radians = angle * pi / 180.0
                 if export:
-                    export_file.write(f'{angle}, {distance} \n')
+                    data_export.append([angle, distance])
                 # Plot the data points on the polar graph
                 ax.plot(radians, distance, "ro", alpha=1)
             if export:
-                messagebox.showinfo("Success","Data exported to " + filename)
                 export = False
+                show_save_menu(data_export)
+                
         # Draw the figure
         ax.figure.canvas.draw()
 
     except Exception as e:
+        # FOR DEBUG ---
+        # if export:
+        #     export = False
+        #     for i in range(0,20):
+        #         data_export.append([i, "test"])
+        #     show_save_menu(data_export)
         print("draw_points error: ", e)
     finally:
         if lidar_program_running:
@@ -130,13 +134,57 @@ def exit():
         time.sleep(1)
         window.quit()
 
-def save_data():
-    global export
-    export = not export
+def save_data(val: list, loc: str):
+    global save_menu
+
+    filename = f'/home/pi/{loc}/export_{datetime.datetime.now().strftime("%d-%m-%Y_%I:%M_%s")}.csv'
+    
+    export_file = open(filename, "w")
+    export_file.write(f'Angle,Distance \n')
+    # Loop through the list of data points
+    for elem in val:
+        export_file.write(f'{elem[0]}, {elem[1]} \n')
+    messagebox.showinfo("Success","Data exported to " + filename, parent=save_menu)
+    save_menu.destroy()
     return
 
+def show_save_menu(val):
+    global save_menu
 
+    
+    print("exporting")
+    helv36 = tkFont.Font(family='Helvetica', size=40, weight=tkFont.BOLD)  # configures the font for the widgets
 
+    disks = subprocess.check_output("cd ~ && ls -d */", shell=True).decode("utf8").replace("/","").split("\n")[0:-1] # uses this command to get the disk names and stores them in a list
+    
+    save_menu = Toplevel(window)
+    popup = Frame(save_menu)
+    popup.place(relx = 0.5,  
+                   rely = 0.5, 
+                   anchor = 'center') 
+    save_menu.geometry(f'{window.winfo_screenwidth()}x{window.winfo_screenheight()}')  # Size of the window 
+    save_menu.title("Export to:")
+    my_str1 = StringVar()
+    l1 = Label(popup,  textvariable=my_str1, width=20, font=tkFont.Font(family='Helvetica', size=30, weight=tkFont.BOLD) )
+    l1.grid(row=1, column=1)
+    my_str1.set("Save to:")
+
+    # listbox with disk names
+    items = Listbox(popup, width=30, height=15, font=tkFont.Font(family='Helvetica', size=30, weight=tkFont.BOLD))
+    ct = 0
+    for disk in disks:
+        items.insert(ct, disk)
+        ct+=1
+
+    items.grid(row=2, column=1)
+    save_button = Button(popup, width=20, height=3, text="Save", command=lambda: save_data(val=val, loc=disks[items.curselection()[0]]), bg="red", fg="white",
+                   font=helv36)
+    save_button.grid(row=3, column=1)
+    
+
+def start_export():
+    global export
+    export = True
 
 def setup_gui():
     # This function setps up the GUI
@@ -160,7 +208,7 @@ def setup_gui():
                   font=helv36)  # stop scan button (TO-DO)
     stop.pack()
 
-    save = Button(buttons, width=20, height=3, command=save_data, text="Save", bg="blue", fg="white", font=helv36)  # save button (TO-DO)
+    save = Button(buttons, width=20, height=3, command=start_export, text="Save", bg="blue", fg="white", font=helv36)  # save button (TO-DO)
     save.pack()
 
     close = Button(buttons, width=20, height=3, text="Exit", command=exit, bg="red", fg="white",
